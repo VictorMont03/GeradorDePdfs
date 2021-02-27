2531b230c135214d20393bbf65b3c4ead2e73153
VictorMont <victormonteir2@gmail.com>
2021-02-26 23:37:15 -0300

    Segundo commit - aplicação gerando pdf

--- src/server.js
+++ src/server.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const ejs = require('ejs');
 const path = require('path');
+const puppeteer = require('puppeteer');
 const app = express();
 
 const passengers = [
@@ -18,16 +19,42 @@ const passengers = [
         name: 'Duarte',
         flightNumber: 1234,
         time: "22h00",
+    },
+    {
+        name: 'André',
+        flightNumber: 5544,
+        time: "12h00",
+    },
+    {
+        name: 'Neymar Jr',
+        flightNumber: 1010,
+        time: "14h00",
     }
 ]
 
+app.get("/pdf", async(request, response) => {
+    const browser = await puppeteer.launch();
+    const page = await browser.newPage();
+    await page.goto("http://localhost:3000/", {
+        waitUntil: 'networkidle0'
+    })
+    const pdf = await page.pdf({
+        printBackground: true,
+        format: 'Letter',
+    });
+    await browser.close();
+    response.contentType("application/pdf")
+    return response.send(pdf)
+})
+
 app.get('/', (request, response) => {
     const filePath = path.join(__dirname, 'print.ejs')
-    ejs.renderFile(filePath, { passengers }, (err, data) => {
+    ejs.renderFile(filePath, { passengers }, (err, html) => {
         if(err) {
             response.send("Erro")
         }
-        return response.send(data)
+        return response.send(html)
+        
     })
 
 })
